775625df272505998eeeac2b3a9b618919582355
Michele Riccardo Esposito <mresposito5@gmail.com>
2020-03-31 13:55:50 +0100

    update naming

--- packages/examples/landing/components/editor/Toolbar/ToolbarSection.tsx
+++ packages/examples/landing/components/editor/Toolbar/ToolbarSection.tsx
@@ -6,7 +6,7 @@ import {
   Grid,
   Divider,
 } from "@material-ui/core";
-import { useNode } from "@craftjs/core";
+import { useNode } from "@candulabs/craft-core";
 import { makeStyles } from "@material-ui/core/styles";
 const usePanelStyles = makeStyles((_) => ({
   root: {
